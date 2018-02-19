782dcc811fe6b84f578f0d67bee6285f998c81c8
Hubert Zhang <hubertzhang@apache.org>
2018-02-19 23:00:22 +0800

    df

---
+++ tests/sql/resgroup_setup.sql
@@ -0,0 +1,2 @@
+create resource group plgroup with (concurrency=0, cpu_rate_limit=10, memory_limit=30, memory_auditor=1);
+create resource group plgroup2 with (concurrency=0, cpu_rate_limit=10, memory_limit=10, memory_auditor=1);
