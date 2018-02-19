03f2794d2dde4791178136dc5f3e7a9ad03b9374
Hubert Zhang <hubertzhang@apache.org>
2018-02-19 17:22:34 +0800

    ss

--- concourse/scripts/run_plcontainer_resgroup_tests.sh
+++ concourse/scripts/run_plcontainer_resgroup_tests.sh
@@ -2,6 +2,40 @@
 
 set -eox pipefail
 
+
+
+
+
+CLUSTER_NAME=$(cat ./cluster_env_files/terraform/name)
+
+if [ "$TEST_OS" = centos6 ]; then
+    CGROUP_BASEDIR=/cgroup
+else
+    CGROUP_BASEDIR=/sys/fs/cgroup
+fi
+
+if [ "$TEST_OS" = centos7 -o "$TEST_OS" = sles12 ]; then
+    CGROUP_AUTO_MOUNTED=1
+fi
+
+
+make_cgroups_dir() {
+    local gpdb_host_alias=$1
+    local basedir=$CGROUP_BASEDIR
+
+    ssh -t $gpdb_host_alias sudo bash -ex <<EOF
+        for comp in cpu cpuacct memory; do
+            chmod -R 777 $basedir/\$comp
+            mkdir -p $basedir/\$comp/gpdb
+            chown -R gpadmin:gpadmin $basedir/\$comp/gpdb
+            chmod -R 777 $basedir/\$comp/gpdb
+        done
+EOF
+}
+
+make_cgroups_dir ccp-${CLUSTER_NAME}-0
+make_cgroups_dir ccp-${CLUSTER_NAME}-1
+
 scp -r plcontainer_gpdb_build mdw:/tmp/
 scp -r plcontainer_src mdw:~/
 ssh mdw "bash -c \" \
@@ -20,7 +54,7 @@ export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
 source /usr/local/greenplum-db-devel/greenplum_path.sh; \
 plcontainer image-add -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-python-images.tar.gz; \
 plcontainer image-add -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-r-images.tar.gz; \
-plcontainer runtime-add -r plc_python_shared -i pivotaldata/plcontainer_python_shared:devel -l python -s use_container_logging=yes -s resource_group_name=plgroup; \
+plcontainer runtime-add -r plc_python_shared -i pivotaldata/plcontainer_python_shared:devel -l python -s use_container_logging=yes; \
 plcontainer runtime-add -r plc_r_shared -i pivotaldata/plcontainer_r_shared:devel -l r -s use_container_logging=yes; \
 plcontainer runtime-add -r plc_python_network -i pivotaldata/plcontainer_python_shared:devel -l python -s use_container_logging=yes -s use_container_network=yes; \
 gpstop -arf; \
